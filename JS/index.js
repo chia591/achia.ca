8813459f375510d238a3958356d521f1bec48e81
Chia Seeds <landscape.chia1@gmail.com>
2025-05-07 23:30:12 -0400

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -36,10 +36,8 @@ const csvUrl = 'https://docs.google.com/spreadsheets/d/1StW1SIvVre2s1GXyEf-xmg6M
         header: true, // Automatically uses the first row as keys
         complete: function(results) {
             console.log("Parsed CSV Data:", results.data);
-            data = results.data.filter(row =>
-                row[""] && row[""].trim() !== ""
-            );
-            console.log("Filtered CVS Data:", results.data);
+            data = results.data.filter(row => row["Kit"] && row["Kit"].trim() !== "");
+            console.log("Filtered CVS Data1:", results.data);
             // Example: Display data in a table
             const container = document.getElementById('data-container');
             results.data.forEach(row => {
